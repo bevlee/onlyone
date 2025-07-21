7b6a277523dc567aae2af7a388b962f9ccd90f4a
bevlee <bevan.lee@live.com.au>
2025-07-22 00:17:55 +1000

    add comments for game data

--- gameserver/data/data.js
+++ gameserver/data/data.js
@@ -1,6 +1,9 @@
 
-
+// Players will be able to choose the category of the word they need to guess
 export const categories = ["animals", "people", "places"];
+
+
+// List of possible words for each category
 export const secretWords = {
   animals: ["dog", "cat", "rabbit", "cheetah"],
   people: ["obama", "eminem", "stalin", "taylor swift", "bruno mars"],
