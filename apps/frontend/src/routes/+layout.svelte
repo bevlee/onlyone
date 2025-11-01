dbc9f96d88a5ece05132ea618d5fab21806b643b
bevlee <bevan.lee@live.com.au>
2025-11-01 14:32:20 +1100

    use mode watcher for simpler dark mode

--- apps/frontend/src/routes/+layout.svelte
+++ apps/frontend/src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-	import ThemeToggle from '$lib/components/ThemeToggle.svelte';
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+	import { toggleMode } from 'mode-watcher';
+	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from 'svelte-sonner';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	import favicon from '$lib/assets/favicon.svg';
 	import '../app.css';
 
 	let { children } = $props();
@@ -15,7 +20,15 @@
 <div class="overflow-none mx-auto w-full max-w-[500px]">
 	<header class="grid h-14 w-full grid-cols-3 border-b bg-orange-500">
 		<div class="flex items-center justify-start pl-2">
-			<ThemeToggle />
+			<Button onclick={toggleMode} variant="outline" size="icon">
+				<SunIcon
+					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
+				/>
+				<MoonIcon
+					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"
+				/>
+				<span class="sr-only">Toggle theme</span>
+			</Button>
 		</div>
 
 		<h1 class="col-start-2 col-end-3 pt-2 text-center text-2xl font-bold text-white">
@@ -27,3 +40,4 @@
 </div>
 
 <Toaster />
+<ModeWatcher />
