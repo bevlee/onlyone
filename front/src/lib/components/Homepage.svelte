e582395b1f08c8754abf7d385a6d1a4a3dce85cf
bevlee <bevan.lee@live.com.au>
2025-11-10 01:25:25 +1100

    add note on case sensitivity for rooms

--- front/src/lib/components/Homepage.svelte
+++ front/src/lib/components/Homepage.svelte
@@ -10,8 +10,11 @@
 </script>
 
 <div class="flex flex-col items-center justify-center">
-	<p class="my-6 leading-7">Please enter the room you want to join:</p>
+	<p class="my-6 text-lg leading-7">Please enter a room name to join:</p>
 
+	<p class="rounded border border-blue-100 bg-blue-50 px-3 py-2 text-sm text-gray-600 opacity-80">
+		Note that room names are case sensitive
+	</p>
 	<form class="m-6 text-center" onsubmit={submit}>
 		<Input bind:value={roomName} class="max-w-xs content-center" />
 		<Button class="mt-6 bg-green-700 text-white hover:bg-green-800" type="submit">Join</Button>
