f0ddabc036bbe66dc0766fe8ede3aa203b4b61c0
bevlee <bevan.lee@live.com.au>
2025-11-14 20:04:12 +1100

    update svelte for oninput bug in firefox

--- front/src/lib/components/Homepage.svelte
+++ front/src/lib/components/Homepage.svelte
@@ -2,7 +2,7 @@
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	const { joinRoom } = $props();
-	let roomName = $state('testRoom');
+	let roomName = $state('');
 	const submit = (e) => {
 		e.preventDefault();
 		joinRoom(roomName);
@@ -10,13 +10,27 @@
 </script>
 
 <div class="flex flex-col items-center justify-center">
-	<p class="my-6 text-lg leading-7">Please enter a room name to join:</p>
+	<p class="mt-6 text-lg leading-7">Please enter a room name to join:</p>
 
-	<p class="rounded border border-blue-100 bg-blue-50 px-3 py-2 text-sm text-gray-600 opacity-80">
-		Note that room names are case sensitive
-	</p>
 	<form class="m-6 text-center" onsubmit={submit}>
-		<Input bind:value={roomName} class="max-w-xs content-center" />
-		<Button class="mt-6 bg-green-700 text-white hover:bg-green-800" type="submit">Join</Button>
+		<Input
+			bind:value={roomName}
+			autocapitalize="on"
+			autocorrect="off"
+			pattern="[A-Z0-9]+"
+			title="Please enter a combination of Uppercase letters and numbers only"
+			placeholder="Enter Room Name"
+			maxlength={10}
+			oninput={(e) => {
+				// Remove special chars and convert to uppercase
+				roomName = roomName.replace(/[^a-zA-Z0-9]/g, '').toUpperCase();
+			}}
+			class="max-w-xs content-center"
+		/>
+		<Button
+			class="mt-6 bg-green-700 text-white hover:bg-green-800"
+			disabled={roomName === ''}
+			type="submit">Join</Button
+		>
 	</form>
 </div>
